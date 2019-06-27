d78b57de1e050422bb2da4584404252a0ce24a04
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-06-26 21:38:48 -0300

    Action types'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Paragraph} from './paragraph';
+import { Paragraph } from './paragraph';
 
 class App extends Component {
-  
+
   /*
     Contrutor 
-  */ 
-  constructor(props){
+  */
+  constructor(props) {
     super(props)
     /**
      * State e props são as caracteristicas 
@@ -16,38 +16,36 @@ class App extends Component {
     this.state = {
       name: "fiap",
       data: "26/06/2019",
-      number:{ prop: 2 }
+      number: { prop: 2 }
     }
     //Correto, uma forma de fazer
     // Aqui utilizamos o `bind` para que o `this` funcione dentro da nossa callback
     this.updateState = this.updateState.bind(this);
     //this.handleSubmit =this.handleSubmit.bind(this);
-   // this.handleChange =this.handleChange.bind(this);
+    // this.handleChange =this.handleChange.bind(this);
   }
 
-  updateState(){
-    this.setState({name:"fullstack"});
+  updateState() {
+    this.setState({ name: "fullstack" });
   }
 
-  handleSubmit(){
+  handleSubmit() {
 
   }
-/*
-  handleChange(event){
-    this.setState({name:event.target.value});
-  }*/
+  /*
+    handleChange(event){
+      this.setState({name:event.target.value});
+    }*/
   render() {
     return (
-      <form className="App" style={{paddingTop:'10px' }} onSubmit={this.handleSubmit}>
-        <div className="App" style={{ paddingTop: '10px' }}>
-          <input type='text' value={this.state.name} />
-          <button onClick={this.updateState} type="button">
-            {this.state.name}
-          </button>
-          <h1 id='texto'>{this.state.name}</h1>
-          <Paragraph variavel={this.state.name}/>
-        </div>
-      </form>
+      <div className="App" style={{ paddingTop: '10px' }}>
+        <input type='text' value={this.state.name} />
+        <button onClick={this.updateState} type="button">
+          {this.state.name}
+        </button>
+        <h1 id='texto'>{this.state.name}</h1>
+        <Paragraph variavel={this.state.name} />
+      </div>
     )
   }
 }
--- src/reducers/clickReducer.js
+++ src/reducers/clickReducer.js
@@ -1,3 +1,16 @@
-export const initialState = {
-    newValue: 'Atualizado via Redux!'
-  };
+import CLICK_UPDATE_VALUE from './actionTypes';
+
+const initialState = {
+  newValue: 'Atualizado via Redux!'
+};
+export const clickReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'CLICK_UPDATE_VALUE':
+      return {
+        ...state,
+        newValue: action.newValue
+      };
+    default:
+      return state;
+  }
+};
